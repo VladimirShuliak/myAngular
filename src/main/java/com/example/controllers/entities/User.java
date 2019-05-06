9d094c04c233f72c97a6d08c28b4eae240a8c274
Vladimir Shuliak <Vovanshuliak@ukr.net>
2019-05-06 08:07:41 +0300

    Initial commit

---
+++ src/main/java/com/example/controllers/entities/User.java
@@ -0,0 +1,32 @@
+package com.example.controllers.entities;
+
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+
+    public User() {
+        this.name = "";
+        this.email = "";
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
